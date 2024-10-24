913230c41a430bd2c4105f3bcbaf5116914990e6
Iqbal <89783237+IqbalManazilYuni@users.noreply.github.com>
2024-10-25 04:07:45 +0700

    pushh

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import ServerlessHttp from "serverless-http";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
@@ -62,3 +63,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports.handler = ServerlessHttp(app);
