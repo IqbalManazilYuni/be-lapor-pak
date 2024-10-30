b228690f6e093a32e531a02b184880707933840a
Iqbal <89783237+IqbalManazilYuni@users.noreply.github.com>
2024-10-30 12:03:51 +0700

    update firebase

--- config/firebase.js
+++ config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = initializeApp({
-  apiKey: "AIzaSyAc_vgbbR5iQoIzXkVX6WpxiMGvh1eoDxY",
-  authDomain: "be-lapor-pak.firebaseapp.com",
-  projectId: "be-lapor-pak",
-  storageBucket: "be-lapor-pak.appspot.com",
-  messagingSenderId: "585516009338",
-  appId: "1:585516009338:web:e49d2a1ddfd6671be678c3",
-  measurementId: "G-2HVCWGSLHZ",
+  apiKey: "AIzaSyCpYorVngNOh7_kvwD3m0LgTehkoThpx_k",
+  authDomain: "backend-laporpaksumbar.firebaseapp.com",
+  projectId: "backend-laporpaksumbar",
+  storageBucket: "backend-laporpaksumbar.appspot.com",
+  messagingSenderId: "459603006760",
+  appId: "1:459603006760:web:b088293a8dd135715c6c1d",
+  measurementId: "G-QV1XWBQXS4"
 });
 const storage = getStorage(firebaseConfig);
 export default storage;
